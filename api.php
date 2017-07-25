8e04ca32302e9bb99ee9919e37ee60a3199af068
qinghuas <30293805+qinghuas@users.noreply.github.com>
2017-07-25 16:44:59 +0800

    修复bug

--- api.php
+++ api.php
@@ -6,24 +6,26 @@ function urlsafe_b64encode($string) {
    return $data;
 }
 
-//登录参数
-$ss_panel_login_address = "";
+//登录配置
+$ss_panel_login_address = '';//ss panel v3 登录地址
 $username = htmlspecialchars($_GET['user']);//用户名
 $password = htmlspecialchars($_GET['passwd']);//密码
-$two_step_verification_code = htmlspecialchars($_GET['code']);//两步验证码
-//数据库参数
+
+//数据库配置
 $db_host = 'localhost';$db_user = 'root';$db_pw = 'root';$db_name = 'sspanel';
+
 //Group名称
 $group_name = '';//Group名称
-$group_name_base64 = urlsafe_b64encode($group_name);//安全base64加密后的Group名称
-//生成连接前缀参数，请勿修改
-$after_obfs = '/?obfsparam=';
-$after_server_name = '&remarks=';
+$group_name_base64 = urlsafe_b64encode($group_name);//安全编码后的Group名称
+
+//链接前缀,无需修改
 $after_group = '&group=';
 $after_ssr_url = 'ssr://';
+$after_obfs = '/?obfsparam=';
+$after_server_name = '&remarks=';
 
-//服务器ID设置,服务器ID可在 "管理面板-节点列表" 中找到
-//$server_jp = '3';
+//服务器ID设置
+//$server_jp = '3';//日本
 
 //验证账号密码正误
 $post_data = array ("email" => "$username","passwd" => "$password","code" => "$two_step_verification_code");
@@ -40,29 +42,30 @@ $login_code = $login_results_json->{'ret'};
 $login_msg = $login_results_json->{'msg'};
 
 switch($login_code){
-//当账号密码正确时,$login_code=1
 case '1':
-//执行生成函数
+//用户名密码正确时
 function get_ssr_url($server_id,$username,$db_host,$db_user,$db_pw,$db_name,$group_name,$group_name_base64,$after_obfs,$after_server_name,$after_group,$after_ssr_url) {
 //获取连接配置
 $con = mysqli_connect($db_host, $db_user, $db_pw, $db_name) or die("Connection error.".mysqli_error());
 $sql = 'SELECT * FROM `user` WHERE `email` = \'' . $username . '\'';
 $user_config = mysqli_fetch_array(mysqli_query($con, $sql));
-$user_passwd = $user_config['passwd'];//密码
-$user_passwd_base64 = urlsafe_b64encode($user_passwd);//安全base64加密后的ssr连接密码
-$user_port = $user_config['port'];//端口
+$user_passwd = $user_config['passwd'];//ssr连接密码
+$user_passwd_base64 = urlsafe_b64encode($user_passwd);//安全编码后的ssr连接密码
+$user_port = $user_config['port'];//ssr连接端口
 $user_protocol = $user_config['protocol'];//协议
 $user_method = $user_config['method'];//加密
+$user_class = $user_config['class'];//用户等级
 $user_obfs = $user_config['obfs'];//混淆
 $user_obfs_parameter = $user_config['obfs_param'];//混淆参数
-$user_obfs_parameter_safe_base64 = urlsafe_b64encode($user_obfs_parameter);//安全base64加密后的混淆参数
+$user_obfs_parameter_safe_base64 = urlsafe_b64encode($user_obfs_parameter);//安全编码后的混淆参数
 
 //获取服务器配置 
 $sql = 'SELECT * FROM `ss_node` WHERE `id` = \'' . $server_id . '\'';
 $server_config = mysqli_fetch_array(mysqli_query($con, $sql));
 $server_address = $server_config['server'];//地址
 $server_name = $server_config['name'];//名称
-$server_name_safe_base64 = urlsafe_b64encode($server_name);//安全base64后的服务器名称
+$server_class = $server_config['node_class'];//服务器等级
+$server_name_safe_base64 = urlsafe_b64encode($server_name);//安全编码后的服务器名称
 
 //生成
 $array = array("$server_address",":","$user_port",":","$user_protocol",":","$user_method",":","$user_obfs",":","$user_passwd_base64","$after_obfs","$user_obfs_parameter_safe_base64","$after_server_name","$server_name_safe_base64","$after_group","$group_name_base64");
@@ -70,23 +73,28 @@ $server_url_1 = implode($array);//此处得到的是连接配置明文
 $server_url_2 = (base64_encode($server_url_1));//此处得到的是被base64加密的连接配置明文
 $array = array("$after_ssr_url","$server_url_2");//将ssr://与被base64加密的连接配置明文拼接
 $server_url_3 = implode($array);//此处得到完整的ssr://链接
-return $server_url_3;
+
+//比较用户等级与服务器等级
+if($server_class<=$user_class){
+	return $server_url_3;
+    }else{
+	$server_url_3 = '';
+	return $server_url_3;
+    }
 }
 
-//生成各服务器连接链接
+//生成
 $server_jp_url = get_ssr_url("$server_jp","$username","$db_host","$db_user","$db_pw","$db_name","$group_name","$group_name_base64","$after_obfs","$after_server_name","$after_group","$after_ssr_url");
 
-//拼接各服务器连接链接,生成最终配置,输出
+//拼接
 $array = array("$server_jp_url");
-$server_all_url_1 = implode($array);//此处得到的是所有完整的ssr://链接
-$server_all_url_2 = (base64_encode($server_all_url_1));//此处得到的是被base64加密后的所有完整的ssr://链接
+$server_all_url_1 = implode($array);
+$server_all_url_2 = (base64_encode($server_all_url_1));
 echo "$server_all_url_2";
 break;
-
-//当账号密码错误时,$login_code=0
+//用户名密码错误时
 case '0':
 echo "登录失败，用户名或密码不正确。";
 break;
 }
-
 ?>
